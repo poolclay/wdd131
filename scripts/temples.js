b19d15cef2c132bf5db9e8e7c2a1ae5042ca06c4
poolclay <rcastillosarmiento@byupathway.edu>
2026-07-16 22:55:10 -0500

    YA

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,12 +1,3 @@
-const gallery = document.querySelector('.gallery');
-
-for (let i = 0; i < 8; i++) {
-    gallery.innerHTML += `
-        <figure>
-            <img src="images/temple-large.jpeg" alt="Large Temple Image" width="300">
-            <figcaption>Temple of Accra, Ghana</figcaption>
-        </figure>`;
-}
 
 const hamburger = document.querySelector(".ham-menu");
 const menu = document.querySelector(".nav-menu");
